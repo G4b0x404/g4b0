38ae963256c20af1679a931b9973a95f0a5293c9
g4b0x404 <percygarcia801@gmail.com>
2026-07-04 21:15:25 -0500

    new changues

--- shblog.config.ts
+++ shblog.config.ts
@@ -28,7 +28,7 @@ const config: ShBlogConfig = {
   siteLang: "en", // Mantenemos la variable interna del tema para evitar conflictos
 
   // Ruta del Favicon
-  favicon: "/favicon7.png",
+  favicon: "/favicon8.png",
 
   // Configuración de páginas
   pages: {
